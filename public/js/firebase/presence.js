ebec130f1cddbc6f9d2a9e2a336c77dfa4cedcc9
unknown <amanda.wisniowski@gmail.com>
2017-11-28 17:52:21 -0600

    everything we did today...

--- public/js/firebase/presence.js
+++ public/js/firebase/presence.js
@@ -1,19 +1,40 @@
 
 
+var config = {
+    apiKey: "AIzaSyCK6hRlRpzJEd8RsN5hozVosxZalbgsQ0M",
+    authDomain: "ice-breaker-app-165bd.firebaseapp.com",
+    databaseURL: "https://ice-breaker-app-165bd.firebaseio.com",
+    projectId: "ice-breaker-app-165bd",
+    storageBucket: "ice-breaker-app-165bd.appspot.com",
+    messagingSenderId: "673248316614"
+  };
+  firebase.initializeApp(config);
 
-function setOnline(uid){
-    var connected = $firebaseObject(connectedRef);
-    var online = $firebaseArray(usersRef.child(uid+'/online'));
-  
-    connected.$watch(function (){
-      if(connected.$value === true){
-        online.$add(true).then(function(connectedRef){
-          connectedRef.onDisconnect().remove();
+
+
+// since I can connect from multiple devices or browser tabs, we store each connection instance separately
+//         any time that connectionsRef's value is null (i.e. has no children) I am offline
+
+        var myConnectionsRef = new Firebase("https://ice-breaker-app-165bd.firebaseio.com" + '/users/:id/connections');
+        // stores the timestamp of my last disconnect (the last time I was seen online)
+
+        // var lastOnlineRef = new Firebase(config.databaseURL + '/users/:id/lastOnline');
+        
+        var connectedRef = new Firebase("https://ice-breaker-app-165bd.firebaseio.com" + '/.info/connected');
+        connectedRef.on('value', function(snap) {
+            if (snap.val() === true) {
+                // We're connected (or reconnected)! Do anything here that should happen only if online (or on reconnect)
+                // add this device to my connections list
+             alert("connected");
+            } else {
+              alert("not connected");
+            }
+        
+            var con = myConnectionsRef.push(true);
+                // when I disconnect, remove this device
+                con.onDisconnect().remove();
+                
         });
-      }
-    });
-  }
-
 
 
 
