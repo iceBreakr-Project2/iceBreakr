91f3566b0a959155ba6aabd107ffa4b935558703
unknown <amanda.wisniowski@gmail.com>
2017-12-02 05:10:12 -0600

    5:00am push

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -105,4 +105,52 @@ module.exports = function(app) {
         res.json(results);
       });    
     });
-  }
+  
+
+  //TAGS:
+//=======
+// Adding tags and gender pref
+app.post("/api/pref", function(req, res) {
+  console.log("\n\n User Data = ");
+  console.log(req.body);
+  console.log("\n");
+
+  db.preferences.create({
+    gender: req.body.gender,
+    sexualPref: req.body.sexualPref,
+    language: req.body.language,
+    interests: req.body.interests
+
+  }).then(function(results) {
+    //results.redirect("/profile");
+    // `results` here would be the newly created preferences
+    console.log("redirecting to /profile");
+  });    
+});
+
+app.get("/api/pref", function(req, res) {
+  db.preferences.findAll({})
+  .then(function(results) {
+    console.log(results); //node ref
+    res.json(results);
+  });
+});
+
+// Deleting a User's Tags
+app.delete("/api/pref/:userpref", function(req, res) {
+  console.log("\n\n User Data = ");
+  console.log(req.body);
+  console.log("\n");
+
+  db.preferences.delete({
+    gender: req.body.gender,
+    sexualPref: req.body.sexualPref,
+    language: req.body.language,
+    interests: req.body.interests
+
+  }).then(function(results) {
+    res.json(results);
+  });    
+});
+
+}
